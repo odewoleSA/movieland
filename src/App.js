3ccab8537dbc29c1d9d3fa326fa885291c0babf5
Odewole Saheed Adesina <odewolesaheed@gmail.com>
2023-08-28 09:19:04 +0100

    Deploying App

--- src/App.js
+++ src/App.js
@@ -1,58 +1,19 @@
-import React, { useState, useEffect } from "react";
-
-import MovieCard from "./MovieCard";
-import SearchIcon from "./search.svg";
+import React from "react";
 import "./App.css";
+import MoviePage from "./components/MoviePage";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// 8edbdd30
-
-const API_URL = "http://www.omdbapi.com?apikey=8edbdd30";
-
-const App = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    searchMovies("Batman");
-  }, []);
-
-  const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
-    const data = await response.json();
-
-    setMovies(data.Search);
-  };
-
+function App() {
   return (
-    <div className="app">
-      <h1>MovieLand</h1>
-
-      <div className="search">
-        <input
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Search for movies"
-        />
-        <img
-          src={SearchIcon}
-          alt="search"
-          onClick={() => searchMovies(searchTerm)}
-        />
-      </div>
-
-      {movies?.length > 0 ? (
-        <div className="container">
-          {movies.map((movie) => (
-            <MovieCard movie={movie} />
-          ))}
-        </div>
-      ) : (
-        <div className="empty">
-          <h2>No movies found</h2>
-        </div>
-      )}
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/movieland" exact element={<MoviePage />} />
+        </Routes>
+      </Router>
+    </>
   );
+
 };
 
 export default App;
